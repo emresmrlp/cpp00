ee72b83804c206a7ab71faa479e368b2e90310db
Yunus Emre Can Sümeralp <ysumeral@k1m38s01.42istanbul.com.tr>
2025-08-24 21:25:35 +0300

    feat: Added ex02 "The Job Of Your Dreams" and refactored parts of ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ysumeral <ysumeral@student.42istanbul.c    +#+  +:+       +#+        */
+/*   By: ysumeral < ysumeral@student.42istanbul.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 15:19:46 by ysumeral          #+#    #+#             */
-/*   Updated: 2025/08/24 09:25:46 by ysumeral         ###   ########.fr       */
+/*   Updated: 2025/08/24 19:25:31 by ysumeral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,24 @@
 #include "Contact.hpp"
 #include <unistd.h>
 
-static std::string	getInputValue(std::string message, std::string value)
+static bool	getInputValue(std::string message, std::string &value)
 {
 	std::cout << message;
 	std::getline(std::cin, value);
 	if (std::cin.eof())
-		exit(0);
+	{
+		std::cout << "\n* End of file detected, exiting PhoneBook..." << std::endl;
+		return false;
+	}
 	if (value.empty())
 	{
 		std::cout << "* Invalid input. Please try again." << std::endl;
 		return getInputValue(message, value);
 	}
-	return (value);
+	return true;
 }
 
-void	addContact(PhoneBook &phoneBook)
+bool	addContact(PhoneBook &phoneBook)
 {
 	std::string	FirstName;
 	std::string	LastName;
@@ -36,71 +39,97 @@ void	addContact(PhoneBook &phoneBook)
 	std::string	PhoneNumber;
 	std::string	DarkestSecret;
 
-	FirstName = getInputValue("Enter first name: ", FirstName);
-	LastName = getInputValue("Enter last name: ", LastName);
-	Nickname = getInputValue("Enter Nickname: ", Nickname);
-	PhoneNumber = getInputValue("Enter phone number: ", PhoneNumber);
-	DarkestSecret = getInputValue("Enter darkest secret: ", DarkestSecret);
+	if (!getInputValue("Enter first name: ", FirstName))
+		return false;
+	if (!getInputValue("Enter last name: ", LastName))
+		return false;
+	if (!getInputValue("Enter Nickname: ", Nickname))
+		return false;
+	if (!getInputValue("Enter phone number: ", PhoneNumber))
+		return false;
+	if (!getInputValue("Enter darkest secret: ", DarkestSecret))
+		return false;
 	phoneBook.addContact(FirstName, LastName, Nickname, PhoneNumber,
 		DarkestSecret);
 	std::cout << "Contact added successfully!" << std::endl;
+	return true;
 }
 
-void	searchContact(PhoneBook &phoneBook)
+bool	searchContact(PhoneBook &phoneBook)
 {
 	std::string index;
 	
 	phoneBook.listContact();
 	std::cout << "Please enter the index of the contact (0 to exit):"
 		<< std::endl;
-	while (std::getline(std::cin, index))
+	while (1)
 	{
 		std::cout << "-> ";
-		if (std::cin.eof())
-			return ;
+		if (!std::getline(std::cin, index))
+		{
+			if (std::cin.eof())
+			{
+				std::cout << "\n* End of file detected, exiting PhoneBook..." << std::endl;
+				return false;
+			}
+		}
 		if (index == "0")
 		{
 			std::cout << "Exiting search..." << std::endl;
-			return ;
+			return true;
 		}
 		else if (index.length() != 1 || index[0] < '1' || index[0] > '8')
 		{
-			std::cout << "* Invalid index! (1-8 only)" << std::endl;
+			std::cout << "\n* Invalid index! (1-8 only)" << std::endl;
 			std::cout << "* Enter index (0) to exit." << std::endl;
 		}
 		else if (index[0] - '0' <= phoneBook.getNumContacts())
 		{
 			phoneBook.getContactInfo(index[0] - '0');
-			return ;
+			return true;
 		}
 		else
-			std::cout << "* Index not found!" << std::endl;
+			std::cout << "\n* Index not found!" << std::endl;
 	}
-	
+	return true;
 }
 
 void openPhoneBook(PhoneBook phoneBook, std::string command)
 {
-	std::cout << "You opened the PhoneBook!" << std::endl;
+	std::cout << "You opened the PhoneBook! (by ysumeral)" << std::endl;
 	std::cout << "Please select a command (ADD, SEARCH, EXIT):"
 		<< std::endl;
-	std::cout << "-> ";
-	while (std::getline(std::cin, command))
+	while (1)
 	{
 		std::cout << "-> ";
+		if (!std::getline(std::cin, command))
+		{
+			if (std::cin.eof())
+			{
+				std::cout << "\n* End of file detected, exiting PhoneBook..."
+					<< std::endl;
+				break ;
+			}	
+		}
 		if (command == "ADD")
-			addContact(phoneBook);
+		{
+			if (!addContact(phoneBook))
+				break;
+		}
 		else if (command == "SEARCH")
-			searchContact(phoneBook);
+		{
+			if (!searchContact(phoneBook))
+				break;
+		}
 		else if (command == "EXIT")
 		{
-			std::cout << "Closing PhoneBook... Goodbye!" << std::endl;
+			std::cout << "Exiting PhoneBook... Goodbye!" << std::endl;
 			break ;
 		}
 		else
 		{
-			std::cout << "Invalid command!" << std::endl;
-			std::cout << "I said (ADD, SEARCH, EXIT) >:(" << std::endl;
+			std::cout << "* Invalid command!" << std::endl;
+			std::cout << "* I said (ADD, SEARCH, EXIT) >:(" << std::endl;
 		}
 	}
 }
